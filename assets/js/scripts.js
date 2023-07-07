00fe6ed970203e1b3254788feb415c6d8b37691a
mikesav <itakesmack@hotmail.com>
2023-07-07 12:26:51 +0100

    Script to lazy load the BG CSS images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,86 +1,113 @@
 /* add nav style on scroll */
-document.addEventListener('DOMContentLoaded', function () {
-  let observer = new IntersectionObserver(function (entries) {
-    if (!entries[0].isIntersecting) {
-      document.querySelector('.top-nav').classList.add('top-nav--bg');
-      document.querySelector('.top-nav__logo').classList.add('top-nav__logo--shrink');
+document.addEventListener('DOMContentLoaded', () => {
+    let observer = new IntersectionObserver(function (entries) {
+        if (!entries[0].isIntersecting) {
+            document.querySelector('.top-nav').classList.add('top-nav--bg');
+            document.querySelector('.top-nav__logo').classList.add('top-nav__logo--shrink');
+        } else {
+            document.querySelector('.top-nav').classList.remove('top-nav--bg');
+            document.querySelector('.top-nav__logo').classList.remove('top-nav__logo--shrink');
+        }
+    }, { threshold: [0.01] });
+    observer.observe(document.querySelector('.intro') || document.querySelector('.page-intro') || document.querySelector('.four-oh-four'));
+
+    /* Now the cookie stuff */
+    let cookieConsent = getCookie('user_cookie_consent');
+    if (cookieConsent === '') {
+        document.getElementById('cookieCard').style.display = 'block';
     } else {
-      document.querySelector('.top-nav').classList.remove('top-nav--bg');
-      document.querySelector('.top-nav__logo').classList.remove('top-nav__logo--shrink');
+        document.getElementById('cookieCard').style.display = 'none';
     }
-  }, { threshold: [0.01] });
-  observer.observe(document.querySelector('.intro') || document.querySelector('.page-intro') || document.querySelector('.four-oh-four'));
+});
 
-  /* Now the cookie stuff */
-  let cookieConsent = getCookie('user_cookie_consent');
-  if (cookieConsent === '') {
-    document.getElementById('cookieCard').style.display = 'block';
-  } else {
-    document.getElementById('cookieCard').style.display = 'none';
-  }
+/**
+ * Experiment Code To Lazy Load CSS BG Images
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    const cssClasses = '.blog-article__image, .blog__card__img, .services__card__top, .services__card__top--kleiderschrank, .services__card__top--homeoffice, .services__card__top--umzug, .questions__overview__icon, .angebot__list__img, .angebot--kleidershrank .angebot__list__img, .angebot--homeoffice .angebot__list__img, .angebot--umzug .angebot__list__img, .angebot--umzug .angebot__list__img, .services__row--erstgespraech .services__row__visual, .services__row__visual, .services__row--design .services__row__visual, .uber-mich__list__img--book, .uber-mich__list__img.uber-mich__list__img--book';
+    let lazyBackgrounds = [].slice.call(document.querySelectorAll(cssClasses));
 
+    // now set the background image prop to none
+    lazyBackgrounds.forEach((entry) => {
+        entry.classList.add('remove-bg-img');
+    });
+
+    if ('IntersectionObserver' in window) {
+        let lazyBackgroundObserver = new IntersectionObserver( (entries, observer) => {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    entry.target.classList.remove('remove-bg-img');
+                    lazyBackgroundObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyBackgrounds.forEach((lazyBackground) => {
+            lazyBackgroundObserver.observe(lazyBackground);
+        });
+    }
 });
 
 /**
  * Show and hide the hamburger menu
  */
 function showHamburgerMenu() {
-  document.getElementsByTagName('body')[0].classList.add('body__no-scroll');
-  let overlay = document.querySelector('.ham-menu');
-  overlay.style.transform = 'translateY(0)';
+    document.getElementsByTagName('body')[0].classList.add('body__no-scroll');
+    let overlay = document.querySelector('.ham-menu');
+    overlay.style.transform = 'translateY(0)';
 }
 
 function closeHamburgerMenu() {
-  document.getElementsByTagName('body')[0].classList.remove('body__no-scroll');
-  let overlay = document.querySelector('.ham-menu');
-  overlay.style.transform = 'translateY(-100%)';
+    document.getElementsByTagName('body')[0].classList.remove('body__no-scroll');
+    let overlay = document.querySelector('.ham-menu');
+    overlay.style.transform = 'translateY(-100%)';
 }
 
 // Create cookie
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = 'expires=' + d.toUTCString();
-  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = 'expires=' + d.toUTCString();
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
 // Delete cookie
 function deleteCookie(cname) {
-  const d = new Date();
-  d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
-  let expires = 'expires=' + d.toUTCString();
-  document.cookie = cname + '=;' + expires + ';path=/';
+    const d = new Date();
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+    let expires = 'expires=' + d.toUTCString();
+    document.cookie = cname + '=;' + expires + ';path=/';
 }
 
 // Read cookie
 function getCookie(cname) {
-  let name = cname + '=';
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    let name = cname + '=';
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return '';
+    return '';
 }
 
 // Set cookie consent
 function acceptCookieConsent(event) {
-  event.stopPropagation();
-  event.preventDefault();
-  deleteCookie('user_cookie_consent');
-  setCookie('user_cookie_consent', 1, 30);
-  closeCookiePopup(event);
+    event.stopPropagation();
+    event.preventDefault();
+    deleteCookie('user_cookie_consent');
+    setCookie('user_cookie_consent', 1, 30);
+    closeCookiePopup(event);
 }
 
 function closeCookiePopup(event) {
-  event.stopPropagation();
-  event.preventDefault();
-  document.getElementById('cookieCard').style.display = 'none';
+    event.stopPropagation();
+    event.preventDefault();
+    document.getElementById('cookieCard').style.display = 'none';
 }
 
